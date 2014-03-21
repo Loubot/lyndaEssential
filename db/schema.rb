90df52d25a05f8402c92b931abce0ce93fa6f504
Louis Angelini <lllouis@yahoo.com>
2014-03-21 16:01:26 +0000

    Run add_customer_id to orders migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140321154948) do
+ActiveRecord::Schema.define(:version => 20140321160015) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20140321154948) do
     t.string   "invoice_number"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.integer  "customer_id"
   end
 
   create_table "users", :force => true do |t|
--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -6,6 +6,7 @@
 #  invoice_number :string(255)
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
+#  customer_id    :integer
 #
 
 require 'test_helper'
