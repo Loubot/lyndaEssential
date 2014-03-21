6bd5086dd7ec07379867e32725ec867f2dd57722
Louis Angelini <lllouis@yahoo.com>
2014-03-21 11:13:43 +0000

    Migrated artist_id to album

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140321110910) do
+ActiveRecord::Schema.define(:version => 20140321111245) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20140321110910) do
     t.datetime "updated_at",   :null => false
     t.integer  "feature"
     t.string   "image_path"
+    t.integer  "artist_id"
   end
 
   create_table "albums_orders", :id => false, :force => true do |t|
