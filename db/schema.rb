75227b1e2eb2b6220f25d833d0e38bcfca692326
Louis Angelini <lllouis@yahoo.com>
2014-03-21 09:02:12 +0000

    Run order migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140321085918) do
+ActiveRecord::Schema.define(:version => 20140321090115) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(:version => 20140321085918) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "orders", :force => true do |t|
+    t.string   "invoice_number"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   create_table "users", :force => true do |t|
