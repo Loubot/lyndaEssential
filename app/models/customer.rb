3e63f74f15afb6f4ab38c1dd1b03a6082e2a75e5
Louis Angelini <lllouis@yahoo.com>
2014-03-21 09:01:33 +0000

    Generate Order model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  address    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Customer < ActiveRecord::Base
   attr_accessible :address, :first_name, :last_name
 end
--- test/unit/customer_test.rb
+++ test/unit/customer_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  address    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
