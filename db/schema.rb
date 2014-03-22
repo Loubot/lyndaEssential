3519d9527c10016aadafe0c4f31e0199781db539
Louis Angelini <lllouis@yahoo.com>
2014-03-22 19:09:35 +0000

    Destroy sessions migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140321182537) do
+ActiveRecord::Schema.define(:version => 20140322165009) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(:version => 20140321182537) do
     t.text     "biography"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "customers", :force => true do |t|
